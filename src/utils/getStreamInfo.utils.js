675a662642b323be58f1dbf685b08dcc420477fe
rishabh-star <risingmerta@gmail.com>
2025-05-05 23:37:28 +0530

    first commit

--- src/utils/getStreamInfo.utils.js
+++ src/utils/getStreamInfo.utils.js
@@ -1,10 +1,33 @@
 import axios from "axios";
 
-export default async function getStreamInfo(animeId,episodeId,serverName,type) {
+export default async function getStreamInfo(animeId, episodeId, serverName, type) {
   const api_url = "https://vimal.animoon.me/api";
+  const fullUrl = `${api_url}/stream?id=${animeId}&ep=${episodeId}&server=${serverName}&type=${type}`;
+
   try {
-    const response = await axios.get(`${api_url}/stream?id=${animeId}?ep=${episodeId}&server=${serverName}&type=${type}`);
-    return response.data.results;
+    const headers = {
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    };
+
+    // First fetch
+    const res1 = await axios.get(fullUrl, { headers });
+    const tracks1 = res1.data?.results?.streamingLink?.tracks || [];
+
+    // Delay to prevent caching overlap
+    await new Promise(r => setTimeout(r, 300));
+
+    // Second fetch
+    const res2 = await axios.get(fullUrl, { headers });
+    const tracks2 = res2.data?.results?.streamingLink?.tracks || [];
+
+    if (JSON.stringify(tracks1) !== JSON.stringify(tracks2)) {
+      alert("Track list changed between requests!");
+      return null;
+    }
+
+    return res1.data.results;
   } catch (error) {
     console.error("Error fetching stream info:", error);
     return error;
