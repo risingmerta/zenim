90fa503eedb5710cdfb4bd3d2a5313b1614e81f8
rishabh-star <risingmerta@gmail.com>
2025-05-07 00:03:15 +0530

    first commit

--- src/app/ad/page.jsx
+++ src/app/ad/page.jsx
@@ -1,7 +1,22 @@
 import Script from "next/script";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Page = () => {
+  const [adVisible, setAdVisible] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const adContainer = document.getElementById("container-89e983895b722d720163a9cbbe699e17");
+      if (adContainer && adContainer.childNodes.length > 0) {
+        setAdVisible(true);
+      } else {
+        setAdVisible(false);
+      }
+    }, 1000); // Give the script some time to inject content
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div>
       <Script
@@ -10,7 +25,12 @@ const Page = () => {
         data-cfasync="false"
         async
       />
-      <div id="container-89e983895b722d720163a9cbbe699e17" />
+      <div id="container-89e983895b722d720163a9cbbe699e17"/>
+      {!adVisible && (
+        <div style={{ textAlign: "center", marginTop: "8px", color: "#999", fontSize: "14px" }}>
+          This ad supports Animoon 💖
+        </div>
+      )}
     </div>
   );
 };
