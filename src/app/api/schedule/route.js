ef40fb1af1ff19c08c116c91fb773747d8d0559f
rishabh-star <risingmerta@gmail.com>
2025-05-11 13:16:16 +0530

    first commit

--- src/app/api/schedule/route.js
+++ src/app/api/schedule/route.js
@@ -1,21 +1,34 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
+import { connectDB } from "@/lib/mongoClient";
 
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
   const date = searchParams.get("date");
 
   if (!date) {
-    return NextResponse.json({ error: "Missing 'date' query parameter" }, { status: 400 });
+    return NextResponse.json(
+      { error: "Missing 'date' query parameter" },
+      { status: 400 }
+    );
   }
 
-  const api_url = "https://vimal.animoon.me/api";
+  // const api_url = "https://vimal.animoon.me/api";
 
   try {
-    const response = await axios.get(`${api_url}/schedule?date=${date}`);
-    return NextResponse.json(response.data.results);
+    // const response = await axios.get(`${api_url}/schedule?date=${date}`);
+    const db = await connectDB();
+    const animeCollection = db.collection("animoon-schedule");
+    const docs = await animeCollection.find({}).toArray();
+    animeDocs = JSON.parse(JSON.stringify(docs));
+
+    const daySchedule = animeDocs.find((entry) => entry._id === date);
+    return NextResponse.json(daySchedule?.schedule);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: "Failed to fetch schedule" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to fetch schedule" },
+      { status: 500 }
+    );
   }
 }
