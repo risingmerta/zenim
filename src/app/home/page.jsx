a344ca410a82686e8b6a3c7143e637e0954c1284
rishabh-star <risingmerta@gmail.com>
2025-05-16 10:00:21 +0530

    first commit

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -8,11 +8,41 @@ export default async function page({ searchParams }) {
     next: { revalidate: 3600 },
   });
   const data = await res.json();
+
+  const {
+    spotlights,
+    trending,
+    topTen: topten,
+    today: todaySchedule,
+    topAiring: top_airing,
+    mostPopular: most_popular,
+    mostFavorite: most_favorite,
+    latestCompleted: latest_completed,
+    latestEpisode: latest_episode,
+    topUpcoming: top_upcoming,
+    recentlyAdded: recently_added,
+    genres,
+  } = data.results;
+
+  const dataToCache = {
+    spotlights,
+    trending,
+    topten,
+    todaySchedule,
+    top_airing,
+    most_popular,
+    most_favorite,
+    latest_completed,
+    latest_episode,
+    top_upcoming,
+    recently_added,
+    genres,
+  };
   const seacrhParam = await searchParams;
   const refer = seacrhParam?.refer;
   return (
     <div>
-      <Home refer={refer} data={data} />
+      <Home refer={refer} data={dataToCache} />
       {refer && <Advertize refer={refer} />}
       <Script
         src="//abackdamstubborn.com/b7/2f/b2/b72fb2e5a32c00a413ee2bb7ea85b317.js"
