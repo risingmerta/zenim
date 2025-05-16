1cd8cffe2ea1460d2b55fa078863b1f136bbfbfd
rishabh-star <risingmerta@gmail.com>
2025-05-16 09:42:06 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -8,10 +8,8 @@ export default async function Page() {
   try {
     const apis = [
       "https://api.animoon.me/api",
-      // "https://api1.animoon.me/api",
       "https://api2.animoon.me/api",
       "https://api3.animoon.me/api",
-      // "https://vimal.animoon.me/api",
     ];
 
     const baseUrl = apis[Math.floor(Math.random() * apis.length)];
