1cb3184099ea91770c513263590daf7f2804f66f
rishabh-star <risingmerta@gmail.com>
2025-05-20 11:13:10 +0530

    first commit

--- src/component/Share/Share.jsx
+++ src/component/Share/Share.jsx
@@ -9,9 +9,28 @@ import {
   MdEmail,
   MdAlternateEmail,
 } from "react-icons/md";
-import { FaXTwitter, FaFacebookF, FaGetPocket, FaLine, FaLinkedinIn, FaPinterest, FaRedditAlien, FaTelegram, FaTumblr, FaViber, FaWhatsapp, FaShareAlt } from "react-icons/fa";
+import {
+  FaXTwitter,
+  FaFacebookF,
+  FaGetPocket,
+  FaLine,
+  FaLinkedinIn,
+  FaPinterest,
+  FaRedditAlien,
+  FaTelegram,
+  FaTumblr,
+  FaViber,
+  FaWhatsapp,
+  FaShareAlt,
+} from "react-icons/fa";
 import { SlSocialVkontakte } from "react-icons/sl";
-import { SiHatenabookmark, SiInstapaper, SiLivejournal, SiOdnoklassniki, SiWorkplace } from "react-icons/si";
+import {
+  SiHatenabookmark,
+  SiInstapaper,
+  SiLivejournal,
+  SiOdnoklassniki,
+  SiWorkplace,
+} from "react-icons/si";
 import {
   EmailShareButton,
   FacebookShareButton,
@@ -35,7 +54,7 @@ import {
   WorkplaceShareButton,
 } from "react-share";
 
-export default function Share({ ShareUrl, arise, style }) {
+export default function Share({ ShareUrl, arise }) {
   const [copied, setCopied] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
@@ -47,7 +66,7 @@ export default function Share({ ShareUrl, arise, style }) {
   };
 
   return (
-    <div className="share-app" style={style}>
+    <div className="share-app">
       <Image width={50} height={50} src={share} alt="share" />
       <div>
         <p className="primary">
@@ -72,7 +91,10 @@ export default function Share({ ShareUrl, arise, style }) {
           )}
         </button>
 
-        <button className="share-icon-button" onClick={() => setShowModal(true)}>
+        <button
+          className="share-icon-button"
+          onClick={() => setShowModal(true)}
+        >
           <FaShareAlt size={20} />
           <span className="button-text large-screen-only">Share</span>
         </button>
@@ -82,28 +104,71 @@ export default function Share({ ShareUrl, arise, style }) {
       {showModal && (
         <div className="share-modal">
           <div className="modal-content">
-            <button className="close-button" onClick={() => setShowModal(false)}>×</button>
+            <button
+              className="close-button"
+              onClick={() => setShowModal(false)}
+            >
+              ×
+            </button>
 
             <div className="share-grid">
-              <WhatsappShareButton url={ShareUrl}><FaWhatsapp /></WhatsappShareButton>
-              <FacebookShareButton url={ShareUrl}><FaFacebookF /></FacebookShareButton>
-              <TelegramShareButton url={ShareUrl}><FaTelegram /></TelegramShareButton>
-              <RedditShareButton url={ShareUrl}><FaRedditAlien /></RedditShareButton>
-              <TwitterShareButton url={ShareUrl}><FaXTwitter /></TwitterShareButton>
-              <EmailShareButton url={ShareUrl}><MdEmail /></EmailShareButton>
-              <HatenaShareButton url={ShareUrl}><SiHatenabookmark /></HatenaShareButton>
-              <InstapaperShareButton url={ShareUrl}><SiInstapaper /></InstapaperShareButton>
-              <LineShareButton url={ShareUrl}><FaLine /></LineShareButton>
-              <LinkedinShareButton url={ShareUrl}><FaLinkedinIn /></LinkedinShareButton>
-              <LivejournalShareButton url={ShareUrl}><SiLivejournal /></LivejournalShareButton>
-              <MailruShareButton url={ShareUrl}><MdAlternateEmail /></MailruShareButton>
-              <OKShareButton url={ShareUrl}><SiOdnoklassniki /></OKShareButton>
-              <PinterestShareButton url={ShareUrl}><FaPinterest /></PinterestShareButton>
-              <PocketShareButton url={ShareUrl}><FaGetPocket /></PocketShareButton>
-              <TumblrShareButton url={ShareUrl}><FaTumblr /></TumblrShareButton>
-              <ViberShareButton url={ShareUrl}><FaViber /></ViberShareButton>
-              <VKShareButton url={ShareUrl}><SlSocialVkontakte /></VKShareButton>
-              <WorkplaceShareButton url={ShareUrl}><SiWorkplace /></WorkplaceShareButton>
+              <WhatsappShareButton url={ShareUrl}>
+                <FaWhatsapp />
+              </WhatsappShareButton>
+              <FacebookShareButton url={ShareUrl}>
+                <FaFacebookF />
+              </FacebookShareButton>
+              <TelegramShareButton url={ShareUrl}>
+                <FaTelegram />
+              </TelegramShareButton>
+              <RedditShareButton url={ShareUrl}>
+                <FaRedditAlien />
+              </RedditShareButton>
+              <TwitterShareButton url={ShareUrl}>
+                <FaXTwitter />
+              </TwitterShareButton>
+              <EmailShareButton url={ShareUrl}>
+                <MdEmail />
+              </EmailShareButton>
+              <HatenaShareButton url={ShareUrl}>
+                <SiHatenabookmark />
+              </HatenaShareButton>
+              <InstapaperShareButton url={ShareUrl}>
+                <SiInstapaper />
+              </InstapaperShareButton>
+              <LineShareButton url={ShareUrl}>
+                <FaLine />
+              </LineShareButton>
+              <LinkedinShareButton url={ShareUrl}>
+                <FaLinkedinIn />
+              </LinkedinShareButton>
+              <LivejournalShareButton url={ShareUrl}>
+                <SiLivejournal />
+              </LivejournalShareButton>
+              <MailruShareButton url={ShareUrl}>
+                <MdAlternateEmail />
+              </MailruShareButton>
+              <OKShareButton url={ShareUrl}>
+                <SiOdnoklassniki />
+              </OKShareButton>
+              <PinterestShareButton url={ShareUrl}>
+                <FaPinterest />
+              </PinterestShareButton>
+              <PocketShareButton url={ShareUrl}>
+                <FaGetPocket />
+              </PocketShareButton>
+              <TumblrShareButton url={ShareUrl}>
+                <FaTumblr />
+              </TumblrShareButton>
+              <ViberShareButton url={ShareUrl}>
+                <FaViber />
+              </ViberShareButton>
+              <VKShareButton url={ShareUrl}>
+                <SlSocialVkontakte />
+              </VKShareButton>
+              <WorkplaceShareButton url={ShareUrl}>
+                <SiWorkplace />
+              </WorkplaceShareButton>
             </div>
           </div>
         </div>
